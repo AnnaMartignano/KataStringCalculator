3e9f11ee057583fa25ea342994347327dcfbdccd
Anna Martignano <martignano.anna@gmail.com>
2017-06-15 13:34:28 +0200

    Refactoring

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -15,12 +15,23 @@ public class StringCalculator {
     public Integer add(String numbers) throws CaratteriNonAmmessiException {
         if (numbers.isEmpty()) {
             return 0;
-        } else if (numbers.contains(",\n")||numbers.contains("\n,")){
+
+        } else if (existInvalidChars(numbers)){
             throw new CaratteriNonAmmessiException();
+
         } else {
-            String[] parts = numbers.split("[,||\n]");
             //caso generico, eslude tutti i caratteri che non sono "digit" [^0-9]
-            return stream(parts).mapToInt(Integer::parseInt).sum();
+            return stream(numbersFor(numbers))
+                    .mapToInt(Integer::parseInt)
+                    .sum();
         }
+    }
+
+    private boolean existInvalidChars(String numbers) {
+        return numbers.contains(",\n")||numbers.contains("\n,");
+    }
+
+    private String[] numbersFor(String numbers) {
+        return numbers.split("[,||\n]");
     }
 }
